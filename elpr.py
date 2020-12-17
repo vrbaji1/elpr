75e61cddc26c44052eeca107cce1e8b000e8d48e
Jindrich Vrba <vrbaji1@uhk.cz>
2020-12-17 19:53:31 +0100

    Blizime se k prvotnimu shapingu zakaznika.

--- elpr.py
+++ elpr.py
@@ -5,14 +5,14 @@
 Popis: Viz. usage()
 Autor: Jindrich Vrba
 Dne: 21.11.2o2o
-Posledni uprava: 14.12.2o2o
+Posledni uprava: 17.12.2o2o
 """
 
 #TODO jen bezdratove zakazniky
 
 import sys, getpass, getopt, signal, fcntl, os, rrdtool
 sys.path.append('/opt/lib')
-import dtb
+import dtb, ssh
 
 #standardni chovani pri CTRL+C nebo ukonceni roury
 signal.signal(signal.SIGPIPE, signal.SIG_DFL)
@@ -39,11 +39,66 @@ class Zakaznik:
   """
   def __init__(self, cislo_smlouvy):
     self.cislo_smlouvy = cislo_smlouvy
-    self.now_stdev = None
+    #garantovane rychlosti
+    self.garant_down = None
+    self.garant_up = None
+    #aktualni rychlosti
+    self.now_down = None
+    self.now_up = None
+    #nove nastavene rychlosti shapingem
+    self.new_down = None
+    self.new_up = None
+    #statistiky
     self.den_rtt = None
+    self.now_stdev = None
     self.now_rtt = None
+
+  #TODO
+  def navrhni_shaping(self):
+    """ Navrhne vhodnou upravu shapingu.
+    """
+    print("\nDEBUG %s" % (self))
+    print("DEBUG pomer zhorseni rtt %.2f" % (self.now_rtt/self.den_rtt))
+    #TODO doladit pomer snizeni
+    #TODO nemelo by se brat jeste v potaz max(?15?,self.den_rtt) ?
+    snizit=self.now_rtt/self.den_rtt/3
+    print("DEBUG pomer znizeni shapingu %.2f" % (snizit))
+    self.new_down=int(max(self.garant_down, self.now_down/snizit))
+    self.new_up=int(max(self.garant_up, self.now_up/snizit))
+    print("DEBUG %s" % (self))
+
+  #TODO
+  def proved_shaping(self):
+    """ Realizuje shaping a zapise aktualni hodnoty do databaze.
+    """
+    #pokud se nic nezmenilo, neni co spoustet
+    if (self.now_down==self.new_down and self.now_up==self.new_up):
+      return
+    print("TODO zmenit rychlost na down:%d, up:%d" % (self.new_down, self.new_up))
+
+    prikaz="""/opt/shaper/add.py change {self.cislo_smlouvy} {self.garant_down} {self.garant_up} {self.new_down} 0 {self.new_up} 0 0 generuj test_vyvoj_eliminace_pretizeni""".format(self=self)
+    print("DEBUG prikaz:%s" % prikaz)
+
+    #errcode = ssh.command("shaper",prikaz)
+    #print(errcode)
+
+
   def __str__(self):
-    return "objekt zakaznik %d: den_rtt:%s, now_rtt:%s, now_stdev:%s" % (self.cislo_smlouvy, self.den_rtt, self.now_rtt, self.now_stdev)
+    popis = """objekt zakaznik %d: den_rtt:%s, now_rtt:%s, now_stdev:%s
+      """ % (self.cislo_smlouvy, self.den_rtt, self.now_rtt, self.now_stdev)
+    popis += """garant_down:%s garant_up:%s now_down:%s now_up:%s
+      """ % (self.garant_down, self.garant_up, self.now_down, self.now_up)
+    if (self.new_down or self.new_up):
+      popis += """new_down:%s new_up:%s
+      """ % (self.new_down, self.new_up)
+    #return """objekt zakaznik %d: den_rtt:%s, now_rtt:%s, now_stdev:%s
+    #garant_down:%d garant_up:%d now_down:%d now_up:%d
+    #""" % (self.cislo_smlouvy, self.den_rtt, self.now_rtt, self.now_stdev
+    #)
+    #return """objekt zakaznik {x.cislo_smlouvy}: den_rtt:{x.den_rtt}, now_rtt:{x.now_rtt}, now_stdev:{x.now_stdev}
+    #  garant_down:{x.garant_down} garant_up:{x.garant_up} now_down:{x.now_down} now_up:{x.now_up}""".format(x=self)
+    return popis.strip()
+
 
 
 def usage(vystup):
@@ -150,7 +205,7 @@ def overit(cursor, cislo_smlouvy):
   down,up=rrd_stat(cursor,cislo_smlouvy)
   #sys.stderr.write("DEBUG poslednich 10m: down=%d, up=%d [kbit]\n" % (down,up))
   #TODO doladit vychozi hodnotu garantovane rychlosti
-  cursor.execute("select CAST(greatest(garant_down,max_down/2) AS UNSIGNED),CAST(greatest(garant_up,max_up/10) AS UNSIGNED),max_down,max_up from zakaznici where cislo_smlouvy=%d" % (cislo_smlouvy))
+  cursor.execute("select CAST(greatest(garant_down,max_down/2) AS UNSIGNED),CAST(greatest(garant_up,max_up/2) AS UNSIGNED),max_down,max_up from zakaznici where cislo_smlouvy=%d" % (cislo_smlouvy))
   row=cursor.fetchone()
   #sys.stderr.write("DEBUG sluzba g_d, g_u, d, u [kbit]: %s\n" % str(row))
   g_d, g_u, d, u = row
@@ -171,9 +226,13 @@ def overit(cursor, cislo_smlouvy):
     return (False, None)
 
   #pokud proslo vsemi kontrolami, je vhodne k rizenemu shapingu
-  zakaznik = Zakaznik(cislo_smlouvy)
-  zakaznik.now_stdev = stdev
-  return (True, zakaznik)
+  z = Zakaznik(cislo_smlouvy)
+  z.now_stdev = stdev
+  z.garant_down = g_d
+  z.garant_up = g_u
+  z.now_down = d
+  z.now_up = u
+  return (True, z)
 
 
 if __name__ == "__main__":
@@ -206,6 +265,12 @@ if __name__ == "__main__":
   #overit(cursor,)
   #sys.exit()
 
+  #z = Zakaznik(150363)
+  #print("DEBUG %s" % (z))
+  #z.new_down="test"
+  #print("DEBUG %s" % (z))
+  #sys.exit()
+
   L_shapovat = []
 
   if not zamek.zamkni():
@@ -230,6 +295,8 @@ if __name__ == "__main__":
       L_shapovat.append(zakaznik)
 
   for zakaznik in L_shapovat:
-    print("DEBUG %s" % (zakaznik))
+    #print("DEBUG %s" % (zakaznik))
+    zakaznik.navrhni_shaping()
+    zakaznik.proved_shaping()
 
   conn.close()
